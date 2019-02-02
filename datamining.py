6cf5895a2f0db2c283b30bab9a0299e8109e34ad
vinaymatcha <m.vinay38@gmail.com>
2019-02-01 23:20:31 -0700

    Minor Assignment Changes

--- datamining.py
+++ datamining.py
@@ -11,13 +11,19 @@ df['ODD'] = (df.Deaths / df.Population).astype(np.float64)
 plt.bar(df['Abbrev'], height = df['ODD'])
 plt.xlabel("State Abbrevation")
 plt.ylabel("Opioid Death Density")
-plt.show()
-plt.savefig("Images/OddForDM")
+# plt.show()
+# plt.savefig("Images/OddForDM")
 n = len(df['Deaths'])
-simm = np.zeros((n, n))
+simm = [[" "] + (df['Abbrev'].values.tolist())]
 for i in range(50):
     p = df['ODD'][i]
-    arr = [abs(p-x) for x in df.ODD]
-    aMax =  np.max(arr)
-    simm[i:,] = np.abs((arr - aMax)/aMax)
-    print(df['Abbrev'][i] , " ", simm[i, 0:5])
+    arr1 = [abs(p-x) for x in df.ODD]
+    # aMax = np.max(arr1)
+    # arr1 = np.abs(arr1-aMax/aMax)
+    # arr1 = [df['Abbrev'][i]] + arr1.tolist()
+    aMax = np.argmax(arr1)
+    arr1[i] = 1
+    arr1[aMax] = 0
+    arr1 = [df['Abbrev'][i]] + arr1
+    simm.append(arr1)
+    print(simm[i])
